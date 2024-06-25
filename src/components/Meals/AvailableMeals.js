8ab841fceb4bfef80f1fb21257fc7f55428887ab
Vineeth05kumar <Vineeth05kumar>
2024-06-25 18:10:05 +0530

    Made the menu table look good

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,6 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -26,10 +29,12 @@ const DUMMY_MEALS = [
 ];
 
 export default function AvailableMeals() {
-  const mealList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} desc={meal.description} price={meal.price}/>);
   return (
-    <div>
-      <ul>{mealList}</ul>
-    </div>
+    <section className={classes.meals}>
+      <Card >
+        <ul>{mealList}</ul>
+      </Card>
+    </section>
   );
 }
