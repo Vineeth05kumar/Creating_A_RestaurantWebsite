651716197afc9a91f2fa430ce07201e416b1c196
Vineeth05kumar <Vineeth05kumar>
2024-06-25 16:46:47 +0530

    Added cart icon in the header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from "./Header.module.css";
 import imageMeal from "../assets/meals.jpg";
+import HeaderCartButton from './HeaderCartButton';
 
 export default function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Your Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={imageMeal} alt="Mealimage" />
