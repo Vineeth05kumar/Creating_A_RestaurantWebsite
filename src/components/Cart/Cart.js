f03abf4395773a1b51540436b2408fde6f17b268
Vineeth05kumar <Vineeth05kumar>
2024-06-28 22:45:28 +0530

    Updated cart functionality to add and remove items from cart and also list

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,32 @@
-// import { useContext } from "react";
+import { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-// import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 
 export default function Cart(props) {
-  // const ctx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
 
+  const  cartItemAddHandler = item =>{
+    cartCtx.addItem({...item,amount:1})
+  };
+  const cartItemRemoveHandler =id =>{
+ cartCtx.removeItem(id);
+  };
+
+  const hasCartItems = cartCtx.items.length > 0;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
-    <ul className={classes['cart-items ']}>
-      {[{ id: "c1", name:'sushi',amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes["cart-items "]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null,item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -18,11 +35,13 @@ export default function Cart(props) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>23.32</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>CLOSE</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          CLOSE
+        </button>
+        {hasCartItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
