fb926a296050b8bc79ebabddc2d6786281b6c13c
Vineeth05kumar <Vineeth05kumar>
2024-06-29 16:07:10 +0530

    Added new bump animation for cart upon adding usinf useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 export default function HeaderCartButton(props) {
+  const [btnIsHigh, setBtnIsHigh] = useState("");
+
   const cartCtx = useContext(CartContext);
-  const numberOfCartitems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartitems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  },0)
-  return <button className={classes.button} onClick={props.onClickOnCart}>
-    <span className={classes.icon}>
+  }, 0);
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHigh(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHigh(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const btnClasses = `${classes.button} ${btnIsHigh ? classes.bump : ""}`;
+  return (
+    <button className={btnClasses} onClick={props.onClickOnCart}>
+      <span className={classes.icon}>
         <CartIcon />
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>{numberOfCartitems}</span>
-  </button>;
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartitems}</span>
+    </button>
+  );
 }
